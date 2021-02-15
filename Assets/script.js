eed60007364420f74ad0fa35d30e35b8cb9f3b7a
J_Kiser <jdkiser228@gmail.com>
2021-02-14 22:53:30 -0500

    updating index page

---
+++ Assets/script.js
@@ -0,0 +1,26 @@
+var timerEl = document.getElementById(".timer")
+
+function timer() {
+    var timeLeft = 60;
+
+    var timeInterval = setInterval(function() {
+
+        if (timeLeft > 1) {
+            // Set the `textContent` of `timerEl` to show the remaining seconds
+            timerEl.textContent = 'Time:' + timeLeft;
+            // Decrement `timeLeft` by 1
+            timeLeft--;
+        } else if (timeLeft === 1) {
+            // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
+            timerEl.textContent = 'Time:' + timeLeft;
+            timeLeft--;
+          } else {
+            // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+            timerEl.textContent = '';
+            // Use `clearInterval()` to stop the timer
+            clearInterval(timeInterval);
+            // Call the `displayMessage()` function
+            displayMessage();
+          }
+        }, 1000);
+      }
