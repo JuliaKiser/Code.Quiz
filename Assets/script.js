14d90e43ca096b9ae6394b49b0def4c1d66696f5
J_Kiser <jdkiser228@gmail.com>
2021-02-16 16:17:11 -0500

    updated questions in js

--- Assets/script.js
+++ Assets/script.js
@@ -1,25 +1,76 @@
 var timerEl = document.getElementById("timer");
 var startButton =document.getElementById("start");
-var scoreBoard = document.getElementsByClassName('score')
+var scoreBoard = document.getElementsByClassName('scores')
 var scoreEl=document.getElementsByClassName('score-num')
 const question = document.querySelector('.quest');
 const choices = Array.from(document.querySelectorAll('.choice-text'));
 
 let currentQuestion ={};
 let acceptAnswer = true;
-let score = 0
-let availableQuestions = []
+let score = 0;
+let availableQuestions = [];
 
 let questions = [
   {
-   question: 'When was the game Candy Land created?'
+   question: 'When was the game Candy Land created?',
    choice1: '1913',
    choice2: '1925',
    choice3: '1942',
-   choice4: '1975'
+   choice4: '1975',
    answer: 3,
-  }
+  },
+  {
+    question: 'What game helped British Prisoners escape the Nazis?',
+    choice1: 'Candy Land',
+    choice2: 'Monopoly',
+    choice3: 'Clue',
+    choice4: 'Jenga',
+    answer: 2,
+   },
+   {
+    question: 'Who invented the game of life?',
+    choice1: 'Aaron James',
+    choice2: 'Samuel Wallace',
+    choice3: 'Eugene Davis',
+    choice4: 'Milton Bradley',
+    answer: 4,
+   },
+   {
+    question: 'What game has skydivers played during a 13,000-foot drop?',
+    choice1: 'Scrabble',
+    choice2: 'Yahtzee',
+    choice3: 'Nintendo Switch',
+    choice4: 'Bingo',
+    answer: 1,
+   },
+   {
+    question: 'What city was pictionary first created?',
+    choice1: 'Seattle',
+    choice2: 'Los Angelos',
+    choice3: 'Austin',
+    choice4: 'Raleigh',
+    answer: 1,
+   },
+   {
+    question: 'What is another name for chutes and ladders?',
+    choice1: 'Boots and Ladders',
+    choice2: 'Snakes and Ladders',
+    choice3: 'Shoes and Ladders',
+    choice4: 'Yo mama and Ladders',
+    answer: 2,
+   },
+   {
+    question: 'Where was Yahtzee first played?',
+    choice1: 'Las Vegas',
+    choice2: 'A Yacht',
+    choice3: 'Underground Bunker',
+    choice4: 'Japan',
+    answer: 2,
+   }
 ]
+const SCORE_POINTS = 100
+constant MAX_QUESTIONS = 7
+
 startButton.addEventListener("click", startQuiz);
 
 var index = 0;
